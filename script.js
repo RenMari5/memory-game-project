2e685b6b573fdb6361cda26f682d90a272350fd0
shelbysue <101065139+shelbysue@users.noreply.github.com>
2022-05-24 20:23:14 -0400

    reset button event listener

--- script.js
+++ script.js
@@ -1,63 +1,43 @@
 const startButton = document.getElementById("start-button");
+const resetButton = document.getElementById("reset-button");
 const gameContainer = document.getElementById("game-container");
-let cards = [
-  "marlee.png",
-  "kitte.png",
-  "cash.png",
-  "crash.png",
-  "winston.png",
-  "yuna.png",
-  "marlee.png",
-  "kitte.png",
-  "cash.png",
-  "crash.png",
-  "winston.png",
-  "yuna.png",
-];
-
-function duplicate(array, duplicator) {
-  let buildDeck = [];
-  for (i = 0; i < array.length; i++) {
-    for (j = 0; j < duplicator; j++) {
-      buildDeck.push(array[i]);
-    }
-  }
-  return buildDeck;
-}
-
-duplicate(cards, 2);
+let cards = ['eevee-memory.jpg', 'eevee-memory.jpg', 'winston-memory-2.jpg', 'winston-memory-2.jpg', 'pudge.jpg', 'pudge.jpg', 'smudge.jpg', 'smudge.jpg'];
 
 function shuffle(array) {
-  let currentIndex = array.length,
-    temporaryValue,
-    randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) {
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
-
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
-
-  return array;
+    return array;
 }
 
 startButton.addEventListener("click", () => {
-  let shuffledCards = shuffle(cards);
-  for (let item of shuffledCards) {
-    let card = document.createElement("img");
-    card.classList.add("images");
-    card.src = `/images/${item}`;
-    gameContainer.appendChild(card);
-  }
-  console.log(shuffledCards);
-});
+    let shuffledCards = shuffle(cards);
+    for (let item of shuffledCards) {
+        let card = document.createElement("img");
+        card.classList.add("images");
+        card.src = `/images/${item}`;
+        gameContainer.appendChild(card);
+    }
+    console.log(shuffledCards);
+})
 
 card.addEventListener("click", (event) => {
-  event.target.classList.add("flip");
-});
+    event.target.classList.add("flip");
+})
+
+resetButton.addEventListener("click", () => {
+    let cardRemove = document.getElementById("cards");
+    shuffledCards.classList.remove("images");
+})
