13350d4068e6c27f8272cd1faa8931a80cd55a81
KJ Johnson <kjohns95@emich.edu>
2022-05-27 18:36:16 -0400

    don't mind me... just messing with the cards again...

--- script.js
+++ script.js
@@ -52,7 +52,7 @@ function createCards(arrayOfCards) {
     let cardBack = document.createElement("img");
     cardBack.classList.add("images");
     cardBack.classList.add("flip-card-back");
-    cardBack.src = "/images/Untitled design.png";
+    cardBack.src = "/images/mutedBackground.png";
 
     flipCard.addEventListener("click", () => {
       cardFront.classList.add("toggle-card");
