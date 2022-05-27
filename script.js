796a8726077667b4df90fcda5cbc25a2599bedfa
Marlee Garcia <marlee.garcia121@gmail.com>
2022-05-26 22:39:29 -0400

    Updated timer to count seconds and minutes on start and go to zero on reset

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ let cards = [
 ];
 let gameCards = document.querySelectorAll(".images");
 
-let [milliseconds, seconds, minutes] = [0, 0, 0];
-let timerRef = document.querySelector(".timerDisplay");
+let [seconds, minutes] = [0, 0];
+let timerRef = document.getElementById("timerDisplay");
 let int = null;
 
 // function duplicate(array, duplicator) {
@@ -84,7 +84,7 @@ startButton.addEventListener("click", () => {
   if (int !== null) {
     clearInterval(int);
   }
-  int = setInterval(displayTimer, 10);
+  int = setInterval(displayTimer, 1000);
 });
 
 // flipCards.forEach((card) => {
@@ -103,35 +103,26 @@ resetButton.addEventListener("click", () => {
   startButton.disabled = false;
 
   clearInterval(int);
-  [milliseconds, seconds, minutes] = [0, 0, 0];
-  // timerRef.innerHTML = "00 : 00";
+  [seconds, minutes] = [0, 0];
+  timerRef.innerHTML = "00 : 00";
 
   return flipCards;
 });
 
 function displayTimer() {
-  milliseconds += 10;
-  if (milliseconds == 1000) {
-    milliseconds = 0;
-    seconds++;
+  seconds += 1;
     if (seconds == 60) {
       seconds = 0;
       minutes++;
       if (minutes == 60) {
         minutes = 0;
       }
+    }
 
       let m = minutes < 10 ? "0" + minutes : minutes;
       let s = seconds < 10 ? "0" + seconds : seconds;
-      let ms =
-        milliseconds < 10
-          ? "00" + milliseconds
-          : milliseconds < 100
-          ? "0" + milliseconds
-          : milliseconds;
 
-      timerRef.innerHTML = `${m} : ${s} : ${ms}`;
-    }
+      timerRef.innerHTML = `${m} : ${s}`;
   }
 
   // //matched cards
@@ -155,4 +146,3 @@ function displayTimer() {
       }
     });
   };
-}
